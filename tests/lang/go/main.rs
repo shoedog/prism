d7e9ae08abfcd0e1ef60f0b56fcd9601eb072a25
Wesley Jinks <wesley.jinks@gmail.com>
2026-08-23 13:52:33 -0600

    p17-fix4: add on-demand R2 collision poles

--- tests/lang/go/main.rs
+++ tests/lang/go/main.rs
@@ -7,6 +7,7 @@ mod common;
 mod concrete_receiver_alias_test;
 mod concrete_receiver_fix2_test;
 mod concrete_receiver_fix3_test;
+mod concrete_receiver_fix4_test;
 mod concrete_receiver_manifest_test;
 mod concrete_receiver_qualified_fix2_test;
 mod concrete_receiver_route_test;
